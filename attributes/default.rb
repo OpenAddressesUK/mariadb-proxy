9ed0b60305ed204ae6a6e5594d229a1819c59c1c
pikesley <github@orgraphone.org>
2014-11-07 12:05:41 +0000

    Stupid

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,8 @@
 default['mariadb-proxy']['database'] = 'dogebase'
 default['mariadb-proxy']['user'] = 'doge'
 default['mariadb-proxy']['password'] = 'suchpassword'
+
+default['mariadb']['server_root_password'] = 'iloverandompasswords'
+default['mariadb']['server_repl_password'] = 'iloverandompasswords'
+default['mariadb']['server_debian_password'] = 'iloverandompasswords'
+default['mariadb']['install']['version'] = '5.5'
